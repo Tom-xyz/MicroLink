2d513f96fb8e65877c6c137d5a76bae92ffb8bd9
Thomas Boyle <4200802+tom-xyz@users.noreply.github.com>
2023-01-29 14:21:32 -0600

    Deleting unused files leftover from the old project structure

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    if os.environ.get('APP_ENV') == 'development':
+    if os.environ.get('APP_ENV') == 'dev':
         DOMAIN_NAME = "http://localhost:8000"
     else:
-        DOMAIN_NAME = "http://short.ly"
+        DOMAIN_NAME = "http://microlink.ly"
--- src/main.py
+++
@@ -1,2 +0,0 @@
-from fastapi import FastAPI
-from views import app
