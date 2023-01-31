bb4914c3e46b7707f5f61fb5f15fcc6f93ac5bd4
Thomas Boyle <4200802+tom-xyz@users.noreply.github.com>
2023-01-30 22:13:31 -0600

    Scope API service dependencies to minimal required

--- src/config.py
+++ src/config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     if os.environ.get('APP_STAGE') == 'dev':
         DOMAIN_NAME = "http://localhost:8000"
-        REDIS_URL = "redis://localhost:6379"
     else:
         DOMAIN_NAME = "http://microlink.ly"
-        REDIS_URL = "redis://redis:6379"
+
+    REDIS_URL = "redis://redis-service:6379"
