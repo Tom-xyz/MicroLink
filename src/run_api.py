2ad93cdac3fd6af766e8084b17b11b3804c041aa
Thomas Boyle <4200802+tom-xyz@users.noreply.github.com>
2023-01-31 18:52:38 -0600

    Update web logic to retrieve API IP and improve the UI experience by making an async form request

--- src/run_api.py
+++ src/run_api.py
@@ -2,4 +2,4 @@ from fastapi import FastAPI
 from src.api.views import router as api_router
 
 app = FastAPI()
-app.include_router(api_router, prefix="/api")
+app.include_router(api_router)
--- src/web/views.py
+++ src/web/views.py
@@ -1,8 +1,13 @@
 from fastapi import APIRouter
 from fastapi.responses import HTMLResponse
+import socket
+from jinja2 import Environment, FileSystemLoader
 
 router = APIRouter()
+env = Environment(loader=FileSystemLoader('src/web/templates'))
 
 @router.get("/")
 def index():
-    return HTMLResponse(content=open("src/web/templates/index.html").read())
+    template = env.get_template('index.html')
+    api_host = socket.gethostbyname("microlink-api")
+    return HTMLResponse(template.render(api_host=api_host))
