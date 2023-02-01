ae0adff9ed73d7e6b194826c77c587736e52b54e
Thomas Boyle <4200802+tom-xyz@users.noreply.github.com>
2023-01-31 21:06:45 -0600

    Correct typo in config.py relating to DOMAIN_NAME

--- src/config.py
+++ src/config.py
@@ -10,6 +10,6 @@ class Config:
         # TODO: Use a real domain name
         # DOMAIN_NAME = "http://microlink.ly"
         # Resolving the domain name to an IP address
-        DOMAIN = read_load_balancer_ip("microlink-web", "default")
+        DOMAIN_NAME = read_load_balancer_ip("microlink-web", "default")
 
     REDIS_URL = "redis://redis-service:6379"
