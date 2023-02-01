76c133b834a407a3bb63feb30dd227733c1eaf84
Thomas Boyle <4200802+tom-xyz@users.noreply.github.com>
2023-01-31 20:50:13 -0600

    Use middleware.cors lib to allow CORS for API requests

--- src/run_api.py
+++ src/run_api.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
-from fastapi_cors import CORS
+from fastapi.middleware.cors import CORSMiddleware
 from src.api.views import router as api_router
 
 app = FastAPI()
-CORS(app)
+
+# TODO: Restrict CORS to the domain name
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(api_router)
