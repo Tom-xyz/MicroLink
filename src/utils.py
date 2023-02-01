d5b2d42052983b18ba55f94eeaa6d59113459f49
Thomas Boyle <4200802+tom-xyz@users.noreply.github.com>
2023-02-01 00:47:26 -0600

    Use correct Kubernetes extension in util resolver function

--- src/utils.py
+++ src/utils.py
@@ -2,8 +2,10 @@
 # Example: https://api.<project-id>.endpoints.cloud.google.com
 def resolve_service_ip(ingress_name, namespace):
     import kubernetes
+    from kubernetes import client as kubernetes_client
+
     kubernetes.config.load_incluster_config()
-    api_ext = kubernetes.client.ExtensionsV1beta1Api()
-    ingress = api_ext.read_namespaced_ingress(ingress_name, namespace)
+    api = kubernetes_client.networking.v1_beta1.ExtensionsV1beta1Api()
+    ingress = api.read_namespaced_ingress(ingress_name, namespace)
     ingress_host = ingress.status.load_balancer.ingress[0].ip
     return ingress_host
